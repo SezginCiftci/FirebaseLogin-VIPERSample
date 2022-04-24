5eb8a25d86fe1f26e03e53d8dc691841ee8e605e
Sezgin Ciftci <sezgin.ciftci@huawei.com>
2022-04-24 10:22:38 +0300

    Initial Commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Sezgin on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
